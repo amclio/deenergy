0651598c7d5b9b1681b660000f8ee5ae8ac5b85e
amclio <lincroe@protonmail.com>
2022-10-08 23:59:55 +0900

    Add deenergy

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,175 +1,176 @@
-import type { ChangeEvent } from 'react'
-import { styled } from '~/libs/stitches'
-import { client } from '~/libs/ky'
+import Chart from 'chart.js/auto'
+import { useEffect, useRef } from 'react'
 
-import type { LoaderFunction } from '@remix-run/cloudflare'
-import { userHueCode } from '~/cookies'
-import { useLoaderData } from '@remix-run/react'
-import type { HueTokenResponse } from '~/interfaces/hue'
-import { useRefreshHueToken } from '~/hooks/hue'
-import { useHueAccessToken, useHueRefreshToken } from '~/stores/hue'
-import { useEffect, useState } from 'react'
-import ky from 'ky-universal'
+import styles from '~/styles/main.css'
 
-interface LoaderData {
-  hueCode: string
-  hueAccessToken: string
-  hueRefreshToken: HueTokenResponse['refresh_token']
-  hueAccessTokenExpiresIn: HueTokenResponse['expires_in']
-  hueClientID: string
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }]
 }
 
-export const loader: LoaderFunction = async ({ request, context }) => {
-  const cookieHeader = request.headers.get('Cookie')
-  const cookie = (await userHueCode.parse(cookieHeader)) || {}
+function Graph() {
+  const ref = useRef<HTMLCanvasElement>(null)
 
-  let { hueCode, hueRefreshToken } = cookie
+  useEffect(() => {
+    if (ref.current) {
+      var L1x = ['7월', '8월', '9월', '10월', '11월', '12월']
+      var L1y = [250, 280, 200, 180, 150, 300]
+      var L2y = [789, 987, 876, 765, 654, 543]
 
-  let hueAccessToken = ''
-  let hueAccessTokenExpiresIn = 0
+      new Chart(ref.current, {
+        // type: 'line',
+        type: 'bar',
+        data: {
+          labels: L1x, //x축
+          datasets: [
+            {
+              label: '2021년', //그래프 이름
+              backgroundColor: '#ff6b6b', //그래프 배경색
+              data: L1y, //y축
+            },
+            {
+              label: '2022년',
+              data: L2y,
+              backgroundColor: '#5c7cfa',
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          scales: {
+            x: {
+              grid: {
+                display: false,
+              },
+            },
+            y: {
+              grid: {
+                display: false,
+              },
+            },
+          },
+        },
+      })
+    }
+  }, [])
 
-  const isTokenInvalid = !cookie.hueRefreshToken
+  if (typeof window === 'undefined') return null
 
-  if (hueCode && isTokenInvalid) {
-    const auths = new URLSearchParams()
-
-    auths.set('grant_type', 'authorization_code')
-    auths.set('code', hueCode)
-    auths.set('client_id', (context.HUE_CLIENT_ID as string) || '')
-    auths.set('client_secret', (context.HUE_CLIENT_SECRET as string) || '')
-
-    const res = await client.post('https://api.meethue.com/v2/oauth2/token', {
-      body: auths,
-    })
-    const { refresh_token, access_token, expires_in } =
-      await res.json<HueTokenResponse>()
-
-    cookie.hueRefreshToken = refresh_token
-    hueAccessToken = access_token
-    hueRefreshToken = refresh_token
-    hueAccessTokenExpiresIn = expires_in
-  }
-
-  const bodyObject: LoaderData = {
-    hueCode,
-    hueAccessToken,
-    hueRefreshToken,
-    hueAccessTokenExpiresIn,
-    hueClientID: context.HUE_CLIENT_ID as string,
-  }
-  const body = JSON.stringify(bodyObject)
-
-  return new Response(body, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Set-Cookie': await userHueCode.serialize(cookie),
-    },
-  })
+  return <canvas id="LineG" ref={ref} style={{ width: '100%' }}></canvas>
 }
 
 export default function Index() {
-  const { hueCode, hueRefreshToken, hueAccessToken, hueClientID } =
-    useLoaderData<LoaderData>()
-  const HUE_CLIENT_ID =
-    typeof window !== 'undefined' ? window.ENV.HUE_CLIENT_ID : hueClientID
-
-  const [refreshToken, setRefreshToken] = useHueRefreshToken()
-  const [accessToken, setAccessToken] = useHueAccessToken()
-
-  useEffect(() => {
-    setRefreshToken(hueRefreshToken)
-    setAccessToken(hueAccessToken)
-  }, [hueAccessToken, hueRefreshToken, setAccessToken, setRefreshToken])
-
-  useRefreshHueToken()
-
-  const [hueConfig, setHueConfig] = useState<any>()
-  const [hueUsername, setHueUsername] = useState<any>()
-  const [hueDevices, setHueDevices] = useState<any[]>([])
-  const [hueCurrentDevice, setHueCurrentDevice] = useState<string>('')
-
-  let client = ky.create({
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-      'hue-application-key': hueUsername,
-    },
-  })
-
-  // Bridge Button Config
-
-  const handleHueConfig = async () => {
-    setHueConfig(await client.put('/api/hue/config').json())
-  }
-
-  // Getting username
-
-  const handleHueUsername = async () => {
-    const data = await client.post('/api/hue/username').json<any>()
-
-    const username = data[0].success.username
-    console.log('~ username', username)
-    setHueUsername(username)
-  }
-
-  // Retreiving devices
-
-  const handleHueDevices = async () => {
-    const res = await client.get('/api/hue/devices').json<any>()
-    const devices = res.data.map((device: { services: any[] }) =>
-      device.services.find((item: { rtype: string }) => item.rtype === 'light')
-    )
-    setHueDevices(devices)
-  }
-
-  const handleSelectDevice = (e: ChangeEvent<HTMLSelectElement>) => {
-    const value = e.target.value
-    setHueCurrentDevice(value)
-  }
-
-  const handleClick = async (value: 'on' | 'off') => {
-    const condition = value === 'on'
-
-    await client.put(`/api/hue/lights/${hueCurrentDevice}`, {
-      json: { on: { on: condition } },
-    })
-  }
-
   return (
     <>
-      <a
-        href={`https://api.meethue.com/v2/oauth2/authorize?client_id=${HUE_CLIENT_ID}&response_type=code&state=1234`}
-      >
-        Hue Login
-      </a>
-      <div>{hueCode}</div>
-      <div>{refreshToken}</div>
-      <div>{accessToken}</div>
+      <header>
+        <div className="backdrop">
+          <div className="container">
+            <div className="title">De에너지</div>
+            <div className="sub">by CTQ from Seil</div>
+          </div>
+        </div>
+      </header>
+      <div className="content">
+        <section className="reserved">
+          <div className="section">
+            <div className="current">지금까지 절약한 에너지</div>
+          </div>
+          <div className="energy highlight">
+            <span className="number">8,883</span>
+            <span className="wh">kWh</span>
+          </div>
+          <div className="check">
+            <div className="lable">에너지 절약 상태 — Good</div>
+          </div>
+        </section>
+        <section className="graph">
+          <div className="header">에너지 절약량</div>
+          <Graph />
+        </section>
+        <section className="compare">
+          <div className="container">
+            <div className="header">2021년 총 절약량</div>
+            <div className="value">
+              <span className="number">10,100</span>
+              <span className="wh">kWh</span>
+            </div>
+          </div>
+          <div className="container">
+            <div className="header">2022년 총 절약량</div>
+            <div className="value highlight">
+              <span className="number">14,100</span>
+              <span className="wh">kWh</span>
+            </div>
+          </div>
+        </section>
+        <section className="devices">
+          <div className="header">기기</div>
+          <div className="container">
+            <div className="card active">
+              <div className="name">전등</div>
+              <div className="status">켜짐</div>
 
-      <button onClick={handleHueConfig}>Config Hue</button>
-      <div>{JSON.stringify(hueConfig)}</div>
+              <div className="icon">
+                <img src="./assets/images/lamp.png" alt="Lamp" />
+              </div>
+            </div>
+            <div className="card">
+              <div className="name">TV</div>
+              <div className="status">꺼짐</div>
+              <div className="icon">
+                <img src="./assets/images/tv.png" alt="Television" />
+              </div>
+            </div>
+            <div className="card">
+              <div className="name">에어컨</div>
+              <div className="status">꺼짐</div>
 
-      <button onClick={handleHueUsername}>Get Username</button>
-      <div>{hueUsername}</div>
+              <div className="icon">
+                <img
+                  src="./assets/images/air-conditioner.png"
+                  alt="Air conditioner"
+                />
+              </div>
+            </div>
+            <div className="card">
+              <div className="name">선풍기</div>
+              <div className="status">꺼짐</div>
 
-      <button onClick={handleHueDevices}>Retrive Devices</button>
+              <div className="icon">
+                <img src="./assets/images/fan.png" alt="Electric fan" />
+              </div>
+            </div>
+            <div className="card">
+              <div className="name">충전기</div>
+              <div className="status">꺼짐</div>
 
-      <select onChange={handleSelectDevice}>
-        {hueDevices.map(
-          (device) =>
-            device && (
-              <option value={device.rid} key={device.rid}>
-                {device.rid}
-              </option>
-            )
-        )}
-      </select>
-      <div>{hueCurrentDevice}</div>
-      <div>
-        <button onClick={() => handleClick('on')}>On</button>
+              <div className="icon">
+                <img src="./assets/images/charger.png" alt="Charger" />
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="more">
+          <div className="container">
+            <div className="box">
+              <div className="sub">더 알아보기</div>
+              <div className="title">이 앱의 스토리를 알고 싶으신가요?</div>
+              <div className="button-box">
+                <a href="/promotion" className="button">
+                  보러 가기
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
-      <div>
-        <button onClick={() => handleClick('off')}>Off</button>
-      </div>
+      <footer>
+        <div className="container">
+          <div className="copyright">© CTQ from Seil.</div>
+          <div className="github">
+            <a href="https://github.com/amclio/energy-refactor">Sources</a>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
---
+++ app/routes/index_hue.tsx
@@ -0,0 +1,175 @@
+import type { ChangeEvent } from 'react'
+import { styled } from '~/libs/stitches'
+import { client } from '~/libs/ky'
+
+import type { LoaderFunction } from '@remix-run/cloudflare'
+import { userHueCode } from '~/cookies'
+import { useLoaderData } from '@remix-run/react'
+import type { HueTokenResponse } from '~/interfaces/hue'
+import { useRefreshHueToken } from '~/hooks/hue'
+import { useHueAccessToken, useHueRefreshToken } from '~/stores/hue'
+import { useEffect, useState } from 'react'
+import ky from 'ky-universal'
+
+interface LoaderData {
+  hueCode: string
+  hueAccessToken: string
+  hueRefreshToken: HueTokenResponse['refresh_token']
+  hueAccessTokenExpiresIn: HueTokenResponse['expires_in']
+  hueClientID: string
+}
+
+export const loader: LoaderFunction = async ({ request, context }) => {
+  const cookieHeader = request.headers.get('Cookie')
+  const cookie = (await userHueCode.parse(cookieHeader)) || {}
+
+  let { hueCode, hueRefreshToken } = cookie
+
+  let hueAccessToken = ''
+  let hueAccessTokenExpiresIn = 0
+
+  const isTokenInvalid = !cookie.hueRefreshToken
+
+  if (hueCode && isTokenInvalid) {
+    const auths = new URLSearchParams()
+
+    auths.set('grant_type', 'authorization_code')
+    auths.set('code', hueCode)
+    auths.set('client_id', (context.HUE_CLIENT_ID as string) || '')
+    auths.set('client_secret', (context.HUE_CLIENT_SECRET as string) || '')
+
+    const res = await client.post('https://api.meethue.com/v2/oauth2/token', {
+      body: auths,
+    })
+    const { refresh_token, access_token, expires_in } =
+      await res.json<HueTokenResponse>()
+
+    cookie.hueRefreshToken = refresh_token
+    hueAccessToken = access_token
+    hueRefreshToken = refresh_token
+    hueAccessTokenExpiresIn = expires_in
+  }
+
+  const bodyObject: LoaderData = {
+    hueCode,
+    hueAccessToken,
+    hueRefreshToken,
+    hueAccessTokenExpiresIn,
+    hueClientID: context.HUE_CLIENT_ID as string,
+  }
+  const body = JSON.stringify(bodyObject)
+
+  return new Response(body, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Set-Cookie': await userHueCode.serialize(cookie),
+    },
+  })
+}
+
+export default function Index() {
+  const { hueCode, hueRefreshToken, hueAccessToken, hueClientID } =
+    useLoaderData<LoaderData>()
+  const HUE_CLIENT_ID =
+    typeof window !== 'undefined' ? window.ENV.HUE_CLIENT_ID : hueClientID
+
+  const [refreshToken, setRefreshToken] = useHueRefreshToken()
+  const [accessToken, setAccessToken] = useHueAccessToken()
+
+  useEffect(() => {
+    setRefreshToken(hueRefreshToken)
+    setAccessToken(hueAccessToken)
+  }, [hueAccessToken, hueRefreshToken, setAccessToken, setRefreshToken])
+
+  useRefreshHueToken()
+
+  const [hueConfig, setHueConfig] = useState<any>()
+  const [hueUsername, setHueUsername] = useState<any>()
+  const [hueDevices, setHueDevices] = useState<any[]>([])
+  const [hueCurrentDevice, setHueCurrentDevice] = useState<string>('')
+
+  let client = ky.create({
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'hue-application-key': hueUsername,
+    },
+  })
+
+  // Bridge Button Config
+
+  const handleHueConfig = async () => {
+    setHueConfig(await client.put('/api/hue/config').json())
+  }
+
+  // Getting username
+
+  const handleHueUsername = async () => {
+    const data = await client.post('/api/hue/username').json<any>()
+
+    const username = data[0].success.username
+    console.log('~ username', username)
+    setHueUsername(username)
+  }
+
+  // Retreiving devices
+
+  const handleHueDevices = async () => {
+    const res = await client.get('/api/hue/devices').json<any>()
+    const devices = res.data.map((device: { services: any[] }) =>
+      device.services.find((item: { rtype: string }) => item.rtype === 'light')
+    )
+    setHueDevices(devices)
+  }
+
+  const handleSelectDevice = (e: ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value
+    setHueCurrentDevice(value)
+  }
+
+  const handleClick = async (value: 'on' | 'off') => {
+    const condition = value === 'on'
+
+    await client.put(`/api/hue/lights/${hueCurrentDevice}`, {
+      json: { on: { on: condition } },
+    })
+  }
+
+  return (
+    <>
+      <a
+        href={`https://api.meethue.com/v2/oauth2/authorize?client_id=${HUE_CLIENT_ID}&response_type=code&state=1234`}
+      >
+        Hue Login
+      </a>
+      <div>{hueCode}</div>
+      <div>{refreshToken}</div>
+      <div>{accessToken}</div>
+
+      <button onClick={handleHueConfig}>Config Hue</button>
+      <div>{JSON.stringify(hueConfig)}</div>
+
+      <button onClick={handleHueUsername}>Get Username</button>
+      <div>{hueUsername}</div>
+
+      <button onClick={handleHueDevices}>Retrive Devices</button>
+
+      <select onChange={handleSelectDevice}>
+        {hueDevices.map(
+          (device) =>
+            device && (
+              <option value={device.rid} key={device.rid}>
+                {device.rid}
+              </option>
+            )
+        )}
+      </select>
+      <div>{hueCurrentDevice}</div>
+      <div>
+        <button onClick={() => handleClick('on')}>On</button>
+      </div>
+      <div>
+        <button onClick={() => handleClick('off')}>Off</button>
+      </div>
+    </>
+  )
+}
